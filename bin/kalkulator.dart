cfb06d1bfcead87bd0568306f33ba7e23c6bfc9b
DanielLieandri21 <dlieandri@gmail.com>
2025-04-10 19:50:55 +0700

    menambahkan logika penjumlahan

--- bin/kalkulator.dart
+++ bin/kalkulator.dart
@@ -1,1 +1,11 @@
+void main() {
+  int angka1 = 10;
+  int angka2 = 5;
 
+  // Fitur Penjumlahan
+  print('Penjumlahan: ${tambah(angka1, angka2)}');
+}
+
+int tambah(int a, int b) {
+  return a + b;
+}
